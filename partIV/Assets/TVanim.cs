f233bdbf3f8371eac55811fe82e476cde17972a5
Varshil Udani <vuda002@aucklanduni.ac.nz>
2014-07-21 15:11:33 +1200

    TV animation
    TV turn on and off with animations and FSM

---
+++ partIV/Assets/TVanim.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TVanim : MonoBehaviour {
+
+	public static bool set_tvOn;
+	
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator>();
+		set_tvOn = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (set_tvOn == false) {
+			anim.SetBool ("tvOn", false);
+		} 
+		else if (set_tvOn == true) {
+			anim.SetBool("tvOn", true);
+		}
+	}
+}
+
